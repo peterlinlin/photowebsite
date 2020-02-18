36461a211940d78f2de5740c9f9b0b330a4d2e90
Peter Lin <peterlin.california@gmail.com>
2020-02-18 00:37:15 -0800

    added javascript to read images from gallery **work in progress still**

---
+++ custom.js
@@ -0,0 +1,53 @@
+
+
+function loadTest(){
+  var folder = "./photos/gallery/"
+  var image = "img101.jpg"
+  for(var i = 0; i < 3; i++){
+    $( "<div class='col-sm-4'>" +
+    "<figure class='photo-frame'>" +
+    "<img src=' " +
+    folder +
+    image +
+    "' class='gallery'" +
+    "/>" +
+    "</figure>" +
+    "</div>" ).appendTo("#gallery");
+  }};
+
+  /*
+  var folder = "images/";
+
+  $.ajax({
+  url : folder,
+  success: function (data) {
+  $(data).find("a").attr("href", function (i, val) {
+  if( val.match(/\.(jpe?g|png|gif)$/) ) {
+  $("body").append( "<img src='"+ folder + val +"'>" );
+  }
+  });
+  }
+  });
+
+  */
+
+  function loadGallery(){
+    var folder = "./photos/gallery/";
+    $.ajax({
+      url : folder,
+      success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+          if( val.match(/\.(jpe?g|png|gif)$/) ) {
+            $("<div class='col-sm-4'>" +
+            "<figure class='photo-frame'>" +
+            "<img src=' " +
+            folder +
+            val +
+            "' class='gallery'/>" +
+            "</figure>" +
+            "</div>" ).appendTo("#gallery");
+          }
+        });
+      }
+    });
+  };
